5d8f780196d8d06113c32226a9694bada9ea3f1f
Clara He <clara.he@newtype.com.tw>
2024-09-12 17:50:03 +0800

    feat: 根據 google 表單上的基因資訊取得相對的 id

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button @click="getSheetData()">Generate Data</button>
+  <input v-model="sessionId" />
+  {{ idList }}
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { ref } from "vue";
+
+const data = ref(null);
+const sessionId = ref("9D47A91103D62528AB0CB8EDC693DE88");
+const idList = ref([]);
+const sheetRequest = new Request(
+  "https://sheets.googleapis.com/v4/spreadsheets/12dUoTPZM5Df0WSFuG5Y5xcpkQYVFT3yPLiSm8duCwd8/values/common_intervals?alt=json&key=AIzaSyDRO8bPY_lKNzk5udMUi9VyvyK6YIvJ_ps"
+);
+
+const getSheetData = async () => {
+  await fetch(sheetRequest)
+    .then((response) => {
+      return response.json();
+    })
+    .then((res) => {
+      data.value = res.values;
+      data.value.splice(0, 1);
+      //取得googleSheet上位置資料後轉換成指定格式
+      data.value = convertPosition(data.value);
+    })
+    .then(() => {
+      //傳入指定格式資料取得該位置的基因
+      getPositionId(data.value);
+    })
+
+    .catch((err) => {
+      console.log("錯誤:", err);
+    });
+};
+const getPositionId = (data) => {
+  idList.value = []
+  data.forEach((item, index) => {
+    setTimeout(async () => {
+      const idRequest = new Request(
+        `http://127.0.0.1:5000/api/getId?position=${item}&sessionId=${sessionId.value}`,
+        { method: "GET", mode: 'cors' }  // 確保是 cors 模式 
+      );
+      await fetch(idRequest)
+        .then((response) => {
+
+          return response.json();
+        })
+        .then((res) => {
+          if (res.id) {
+            idList.value.push(res.id);
+          }
+
+        })
+        .catch((err) => {
+          console.log("id取得錯誤:", err);
+          return
+        });
+    }, 10000 * index);
+  });
+};
+const convertPosition = (data) => {
+  return data.map((item) => {
+    return `Ch${item[0]}:${parseInt(item[1]) - 5000}...${parseInt(item[1]) + 5000
+      }`;
+  });
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
